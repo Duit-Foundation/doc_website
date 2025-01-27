12152a0c29ec59a291bb3718c6c21140f5805907
Anatoliy Vasilkov <xaylpq@gmail.com>
2025-01-27 21:34:15 +0300

    fix svg design

--- src/components/HomePage/OpportunitySection/card6/index.tsx
+++ src/components/HomePage/OpportunitySection/card6/index.tsx
@@ -9,6 +9,21 @@ export default function Card1(): JSX.Element {
   return (
     <div className={styles.container}>
       <Contacts className={styles.img} role="img" />
+      <img
+        src="img/HomePage/OpportunitySection/card6/Avatar-3.png"
+        alt="A"
+        className={styles.img1}
+      />
+      <img
+        src="img/HomePage/OpportunitySection/card6/Avatar-2.png"
+        alt="B"
+        className={styles.img2}
+      />
+      <img
+        src="img/HomePage/OpportunitySection/card6/Avatar.png"
+        alt="C"
+        className={styles.img3}
+      />
       <Background className={styles.bg} role="img" />
     </div>
   );
