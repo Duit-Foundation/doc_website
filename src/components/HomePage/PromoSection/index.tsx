aa49d3b86dceaa01e8a39c15762a4971cc608bb3
Pavel Lapscin <papay88@gmail.com>
2025-02-18 16:42:19 +0300

    - Fix mainSection - Fix PromoSection

--- src/components/HomePage/PromoSection/index.tsx
+++ src/components/HomePage/PromoSection/index.tsx
@@ -7,19 +7,19 @@ export default function PromoSection(): JSX.Element {
     <section className={styles.features}>
       <div className={`container ${styles.container}`}>
         <div className="row">
-          <div className={`${styles.title} col`}>
+          <div className={`${styles.title} col col--7`}>
             <Heading as="h1" className={styles.heading}>
               Ваше <span className={styles.yellow}>приложение</span> живет на
               сервере
             </Heading>
-            <p>
+            <p className={styles.description}>
               Вы определяете элементы, состояния и анимации на серверной
               стороне, и они отправляются клиенту независимо от версии
               приложения. Обновляйте UI приложения по мере необходимости — будь
               то каждый час или даже чаще.
             </p>
           </div>
-          <div className="col">
+          <div className="col col--5">
             <Chat />
           </div>
         </div>
