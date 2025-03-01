aa31bc90e91033fb269c519524b3e1c7b2ba3a7d
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-03-01 16:52:48 +0200

    Added locale dropdown

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://duit.pro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -89,6 +89,10 @@ const config: Config = {
         //   to: '/about',
         //   label: 'О проекте',
         // },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
         {
           href: 'https://t.me/duit_community',
           'aria-label': 'Telegram',
